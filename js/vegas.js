a94de857398a9f33f69dd3da3ba5986dada917ff
Geunil Lee <leegun1@gmail.com>
2023-11-27 02:26:02 +0900

    add campaign

--- js/vegas.js
+++ js/vegas.js
@@ -1,14 +1,18 @@
-$(function() {
-    $('#mainimg').vegas({
-        slides: [
-            { src: './images/1.jpg' },	//1枚目の写真指定
-            { src: './images/2.jpg' },	//2枚目の写真指定
-            { src: './images/3.jpg' },	//3枚目の写真指定
-        ],
-		transition: 'blur',			//https://vegas.jaysalvat.com/documentation/transitions/から好みのtransitionを選んで置き換えられます。
-		animation: 'kenburns',		//https://vegas.jaysalvat.com/documentation/transitions/から好みのanimationを選んで置き換えられます。
-		delay: 6000,				//次の画像を表示するまでの時間
-		animationDuration: 10000,	//アニメーション間の引き継ぎタイミング。
-		timer: false,				//プログレスバーを非表示に。
-    });
+$(function () {
+  $("#mainimg").vegas({
+    slides: [
+      { src: "./images/1.jpg" },
+      { src: "./images/2.jpg" },
+      { src: "./images/3.jpg" },
+      { src: "./images/4.jpg" },
+      { src: "./images/5.jpg" },
+      { src: "./images/6.jpg" },
+      { src: "./images/7.jpg" },
+    ],
+    transition: "blur", //https://vegas.jaysalvat.com/documentation/transitions/から好みのtransitionを選んで置き換えられます。
+    animation: "kenburns", //https://vegas.jaysalvat.com/documentation/transitions/から好みのanimationを選んで置き換えられます。
+    delay: 6000, //次の画像を表示するまでの時間
+    animationDuration: 10000, //アニメーション間の引き継ぎタイミング。
+    timer: false, //プログレスバーを非表示に。
+  });
 });
